d157f499b6a55745762394ce798d10681318c641
Gabriel Vullioud <gabriel.vullioud@gmail.com>
2022-06-28 17:03:07 -0300

    remove firebase

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import Footer from "./components/Footer";
 import { BrowserRouter } from "react-router-dom";
 import Main from "./components/Main";
 import { MyProvider } from "./components/CartContext";
-import {app} from "./firebase";
-
-console.log(app);
 
 const App = () =>{
 
