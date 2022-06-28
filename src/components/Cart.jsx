16b48074d6766fcbe6b2d8aaba4b26c2cb089421
Gabriel Vullioud <gabriel.vullioud@gmail.com>
2022-06-28 18:22:42 -0300

    add Cart rendering

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -12,18 +12,23 @@ const Cart = () => {
 
     const CartItems = concerts.map((concert) => {
     console.log(concert.name);
-    
+
     return(
-      <div key = {concert.id}>
-        <h2>{concert.name}</h2>
-        <p>{concert.description}</p>  
-        <h3>Tickets purchased: {concert.q}</h3>
+      <div key={concert.id} className="cartItems">
+      <h3>{concert.name}</h3>
+      <p>{concert.description}</p>
+      <h3>Price per ticket: {concert.price}</h3>
+      <h3>Tickets purchased: {concert.q}</h3>
+      <h3>Total: {concert.price*concert.q}</h3>
       </div>
     )
-  })
+
+  })  
 
   return(
-    <h1>Something</h1>
+    <>
+    <div>{CartItems}</div>
+    </>
   )
 
 }
